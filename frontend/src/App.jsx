9c1576b68e1641bd13bf745112196d074002cab7
Jonathan <juno9593@gmail.com>
2025-03-29 02:15:09 -0400

    initialize frontend with React, Vite, and Tailwind CSS setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+	return (
+		<>
+			<div className='bg-red-50'>
+				Yall ready for a hackathon
+				<span className='bg-blue-700'>aiusdgaisd</span>
+			</div>
+		</>
+	);
+}
+
+export default App;
